05e518bafd1cd673c98c263ae4c08bd3fbc3cc85
madhavmittal330 <madhavmittal330@gmail.com>
2024-11-24 17:09:17 +0530

    Create Room.java

---
+++ Room.java
@@ -0,0 +1,21 @@
+class Room
+{
+	int length; 
+	int breadth;
+	void getdata(int a,int b) 
+	{
+	length=a;
+	breadth=b;
+	}
+}
+class RoomArea
+{
+public static void main(String args[])
+{
+	int area; 
+	Room room1=new Room(); 
+	room1.getdata(14,10); 
+  	area=room1.length*room1.breadth;
+	System.out.println("AREA="+area);
+}
+}
